d011ecd4bb2fb588c368f3c36fa26ce3a9865d27
arthur13stalberg09silva07 <arthur.stalberg@germinare.org.br>
2025-02-25 08:43:31 -0300

    add rpa

---
+++ RPA.py
@@ -0,0 +1,27 @@
+# import win32com.client as win32
+# outlook = win32.Dispatch("outlook.application")
+# email_out = outlook.CreateItem(0)
+import win32com.client as win32
+import os
+
+outlook = win32.Dispatch("outlook.application")
+
+email_out = outlook.CreateItem(0)
+    
+email_out.To = "email.teste@germinare.org.br"
+email_out.Subject = f"PIASSO VAMO TOCAR JUNTOS - Email"  
+
+email_out.HTMLBody = f"""
+    <html>
+        <body>
+            <p>
+                <a href="" target="_blank">Clique aqui para assistir ao vídeo</a>
+            </p>
+        </body>
+    </html>
+    """
+
+email_out.Send()
+print("E-mail enviado com sucesso!")
+
+print("Todos os e-mails foram enviados!")
